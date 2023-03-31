06d2fb46a91585a961e1d544d57c8d6abe5d7947
softmarshmallow <woojoo@softmarshmallow.com>
2023-03-31 14:16:44 +0900

    clean logs

--- figma_archiver/images.py
+++ figma_archiver/images.py
@@ -203,7 +203,7 @@ def image_queue_handler(img_queue: queue.Queue, batch=20, timeout=1800):
         timeout_time = batch_start_time + timeout
         url = None
 
-        progress = tqdm(total=img_queue.qsize(), desc=f"[QUEUED] Archiving Images.. (total: {total})", position=9, leave=False)
+        progress = tqdm(total=img_queue.qsize(), desc=f"[QUEUED] Images (total: {total})", position=9, leave=False)
 
         while len(items_to_process) < batch:
             try:
@@ -213,7 +213,7 @@ def image_queue_handler(img_queue: queue.Queue, batch=20, timeout=1800):
 
                 items_to_process.append((url, path))
                 total += 1
-                progress.desc = f"[QUEUED] Archiving Images.. (total: {total} batch: {len(items_to_process)}/{batch})"
+                progress.desc = f"[QUEUED] Images (total: {total} batch: {len(items_to_process)}/{batch})"
                 batch_start_time = time.time()  # Update the batch start time
             except queue.Empty:
                 if time.time() > timeout_time:
@@ -230,7 +230,7 @@ def image_queue_handler(img_queue: queue.Queue, batch=20, timeout=1800):
         if not items_to_process:
             break
 
-        progress.desc = f"[......] Archiving Images.. (total: {total} batch: {len(items_to_process)}/{batch})"
+        progress.desc = f"[ARCHIVING] Images (total: {total} batch: {len(items_to_process)}/{batch})"
         with ThreadPoolExecutor(max_workers=batch) as executor:
             download_func = partial(download_image_with_progress_bar, progress=progress)
             executor.map(download_func, items_to_process)
