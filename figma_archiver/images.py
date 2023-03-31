eef1b229f8d19efdf1c054e2037ff684cfcf82ab
Universe <woojoo@softmarshmallow.com>
2023-03-31 11:19:25 +0900

    extract file reader

--- figma_archiver/images.py
+++ figma_archiver/images.py
@@ -45,158 +45,77 @@ def main(dir, format, scale, depth, skip_canvas, no_fills, figma_token, source_d
         subdir.mkdir(parents=True, exist_ok=True)
 
         json_file = _src_dir / Path(json_file)
+        file_data = read_file_data(json_file)
 
-        if json_file.is_file():
-            tqdm.write(f"Processing directory {subdir}")
+        if file_data:
+          tqdm.write(f"Processing directory {subdir}")
+          if depth is not None:
+              depth = int(depth)
+          # fetch and save thumbnail (if not already downloaded)
+          if not (subdir / "thumbnail.png").is_file():
+            thumbnail_url = file_data["thumbnailUrl"]
+            download_image(thumbnail_url, subdir / "thumbnail.png")
+            tqdm.write(f"Saved thumbnail to {subdir / 'thumbnail.png'}")
 
-            try:
-              with open(json_file, "r") as file:
-                file_data = json.load(file)
-            except json.decoder.JSONDecodeError as e:
-              tqdm.write(f"Error loading {json_file} Skipping... (Malformed JSON file)) - error: {e.msg} {e.args}")
+          node_ids = get_node_ids(
+              file_data, depth=depth, skip_canvas=skip_canvas)
+          # ----------------------------------------------------------------------
+          # image fills
+          if not no_fills:
+            images_dir = subdir / "images"
+            images_dir.mkdir(parents=True, exist_ok=True)
+            existing_images = os.listdir(images_dir)
 
-              # read the json file and print the start and end of it for debugging
-              with open(json_file, "r") as file:
-                    txt = file.read()
-                    _first_few = txt[0: 100]
-                    _last_few = txt[-100:]
-                    tqdm.write(f"First few characters: \n{_first_few}")
-                    tqdm.write(f"Last few characters: \n{_last_few}")
-
-            if file_data:
-              if depth is not None:
-                  depth = int(depth)
-              # fetch and save thumbnail (if not already downloaded)
-              if not (subdir / "thumbnail.png").is_file():
-                thumbnail_url = file_data["thumbnailUrl"]
-                download_image(thumbnail_url, subdir / "thumbnail.png")
-                tqdm.write(f"Saved thumbnail to {subdir / 'thumbnail.png'}")
-
-              node_ids = get_node_ids(
-                  file_data, depth=depth, skip_canvas=skip_canvas)
-              # ----------------------------------------------------------------------
-              # image fills
-              if not no_fills:
-                images_dir = subdir / "images"
-                images_dir.mkdir(parents=True, exist_ok=True)
-                existing_images = os.listdir(images_dir)
-
-                # TODO: this is not safe. the image fills still can be not complete if we terminate during the download
-                # Fetch and save image fills (B)
-                if not any(not re.match(r"\d+:\d+", img) for img in existing_images):
-                    tqdm.write("Fetching image fills...")
-                    image_fills = fetch_file_images(key, token=figma_token)
-                    url_and_path_pairs = [
-                        (url, os.path.join(images_dir, f"{hash_}.{format}"))
-                        for hash_, url in image_fills.items()
-                    ]
-                    fetch_and_save_images(url_and_path_pairs, position=7)
-                else:
-                    tqdm.write("Image fills already fetched")
-
-              # ----------------------------------------------------------------------
-              # bakes
-              images_dir = subdir / "bakes"
-              images_dir.mkdir(parents=True, exist_ok=True)
-              existing_images = os.listdir(images_dir)
-
-              # Fetch and save layer images (A)
-              node_ids_to_fetch = [
-                  node_id
-                  for node_id in node_ids
-                  if f"{node_id}.{format}" not in existing_images
-                  and f"{node_id}@{scale}x.{format}" not in existing_images
-              ]
-
-              if node_ids_to_fetch:
-                  tqdm.write(
-                      f"Fetching {len(node_ids_to_fetch)} of {len(node_ids)} layer images...")
-                  layer_images = fetch_node_images(
-                      key, node_ids_to_fetch, scale, format, token=figma_token)
-                  url_and_path_pairs = [
-                      (
-                          url,
-                          os.path.join(
-                              images_dir,
-                              f"{node_id}{'@' + str(scale) + 'x' if scale != '1' else ''}.{format}",
-                          ),
-                      )
-                      for node_id, url in layer_images.items()
-                  ]
-                  fetch_and_save_images(url_and_path_pairs, position=8)
-              else:
-                  tqdm.write("Layer images already fetched")
-
-              tqdm.write("All images fetched and saved successfully")
+            # TODO: this is not safe. the image fills still can be not complete if we terminate during the download
+            # Fetch and save image fills (B)
+            if not any(not re.match(r"\d+:\d+", img) for img in existing_images):
+                tqdm.write("Fetching image fills...")
+                image_fills = fetch_file_images(key, token=figma_token)
+                url_and_path_pairs = [
+                    (url, os.path.join(images_dir, f"{hash_}.{format}"))
+                    for hash_, url in image_fills.items()
+                ]
+                fetch_and_save_images(url_and_path_pairs, position=7)
             else:
-              tqdm.write(f"Skipping directory {subdir}: JSON file is empty")
+                tqdm.write("Image fills already fetched")
 
+          # ----------------------------------------------------------------------
+          # bakes
+          images_dir = subdir / "bakes"
+          images_dir.mkdir(parents=True, exist_ok=True)
+          existing_images = os.listdir(images_dir)
+
+          # Fetch and save layer images (A)
+          node_ids_to_fetch = [
+              node_id
+              for node_id in node_ids
+              if f"{node_id}.{format}" not in existing_images
+              and f"{node_id}@{scale}x.{format}" not in existing_images
+          ]
+
+          if node_ids_to_fetch:
+              tqdm.write(
+                  f"Fetching {len(node_ids_to_fetch)} of {len(node_ids)} layer images...")
+              layer_images = fetch_node_images(
+                  key, node_ids_to_fetch, scale, format, token=figma_token)
+              url_and_path_pairs = [
+                  (
+                      url,
+                      os.path.join(
+                          images_dir,
+                          f"{node_id}{'@' + str(scale) + 'x' if scale != '1' else ''}.{format}",
+                      ),
+                  )
+                  for node_id, url in layer_images.items()
+              ]
+              fetch_and_save_images(url_and_path_pairs, position=8)
+          else:
+              tqdm.write("Layer images already fetched")
+
+          tqdm.write("All images fetched and saved successfully")
         else:
-            tqdm.write(
-                f"Skipping directory {subdir}: JSON file not found at '{json_file}'")
-
-
-def log_error(msg, print=True):
-    if print:
-        tqdm.write(msg)
-
-    # check if err log file exists
-    err_log_file = Path("err.log")
-    if not err_log_file.exists():
-        with open(err_log_file, "w") as f:
-            f.write("")
-            f.close()
-    
-    with open(err_log_file, "a") as f:
-        f.write(msg + "\n")
-        f.close()
-
-
-def read_file_data(file_key):
-    with open(os.path.join(file_key, f"{file_key}.json")) as f:
-        return json.load(f)
-
-
-
-def get_node_ids(data, depth=None, skip_canvas=True):
-    """
-
-    In most cases, you want to set depth to 1 or None with skip_canvas=True.
-
-    depth:
-     - None means no limit
-     - 0 means no nodes, retrurns only canvas ids (if skip_canvas is False) otherwise empty list
-     - 1 means only canvas ids (if skip_canvas is False) and direct children of canvas
-
-    ...
-    """
-    def extract_ids_recursively(node, current_depth):
-        if depth is not None and current_depth >= depth:
-            return []
-
-        ids = [node["id"]]
-        if "children" in node:
-            for child in node["children"]:
-                ids.extend(extract_ids_recursively(child, current_depth + 1))
-        return ids
-
-    if skip_canvas:
-        return [
-            id_
-            for canvas in data["document"]["children"]
-            for child in canvas['children']
-            for id_ in extract_ids_recursively(child, 0)
-        ]
-
-    return [
-        id_
-        for child in data["document"]["children"]
-        for id_ in extract_ids_recursively(child, 0)
-    ]
-
-
-def get_existing_images(images_dir):
-    return set(os.listdir(images_dir))
+          tqdm.write(
+                f"Skipping directory {subdir}: Valid JSON file not found at '{json_file}'")
 
 
 def requests_retry_session(
@@ -245,8 +164,6 @@ def fetch_and_save_images(url_and_path_pairs, position=0, num_threads=20):
                 tqdm.write(f"Downloaded {url} to local directory {downloaded_path}")
             else:
                 tqdm.write(f"Failed to download image: {url}")
-
-
 
 
 def fetch_file_images(file_key, token):
@@ -358,6 +275,89 @@ def fetch_node_images(file_key, ids, scale, format, token):
     return image_urls
 
 
+# utils
 
+def log_error(msg, print=True):
+    if print:
+        tqdm.write(msg)
+
+    # check if err log file exists
+    err_log_file = Path("err.log")
+    if not err_log_file.exists():
+        with open(err_log_file, "w") as f:
+            f.write("")
+            f.close()
+    
+    with open(err_log_file, "a") as f:
+        f.write(msg + "\n")
+        f.close()
+
+
+def read_file_data(file: Path):
+    if file.is_file():
+        try:
+          with open(file, "r") as file:
+            file_data = json.load(file)
+            return file_data
+        except json.decoder.JSONDecodeError as e:
+          tqdm.write(f"Error loading {file} Skipping... (Malformed JSON file)) - error: {e.msg} {e.args}")
+
+          # read the json file and print the start and end of it for debugging
+          with open(file, "r") as file:
+                txt = file.read()
+                _first_few = txt[0: 100]
+                _last_few = txt[-100:]
+                tqdm.write(f"First few characters: \n{_first_few}")
+                tqdm.write(f"Last few characters: \n{_last_few}")
+          return None
+    else:
+        tqdm.write(f"File {file} not found")
+        return None
+
+
+
+def get_node_ids(data, depth=None, skip_canvas=True):
+    """
+
+    In most cases, you want to set depth to 1 or None with skip_canvas=True.
+
+    depth:
+     - None means no limit
+     - 0 means no nodes, retrurns only canvas ids (if skip_canvas is False) otherwise empty list
+     - 1 means only canvas ids (if skip_canvas is False) and direct children of canvas
+
+    ...
+    """
+    def extract_ids_recursively(node, current_depth):
+        if depth is not None and current_depth >= depth:
+            return []
+
+        ids = [node["id"]]
+        if "children" in node:
+            for child in node["children"]:
+                ids.extend(extract_ids_recursively(child, current_depth + 1))
+        return ids
+
+    if skip_canvas:
+        return [
+            id_
+            for canvas in data["document"]["children"]
+            for child in canvas['children']
+            for id_ in extract_ids_recursively(child, 0)
+        ]
+
+    return [
+        id_
+        for child in data["document"]["children"]
+        for id_ in extract_ids_recursively(child, 0)
+    ]
+
+
+def get_existing_images(images_dir):
+    return set(os.listdir(images_dir))
+
+
+
+# main
 if __name__ == "__main__":
     main()
