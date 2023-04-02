e80a1f284ec334480bc51165cb78f544bca12863
softmarshmallow <woojoo@softmarshmallow.com>
2023-04-02 15:51:51 +0900

    optimized

--- figma_archiver/scripts/minify.py
+++ figma_archiver/scripts/minify.py
@@ -34,11 +34,19 @@ def minify_json_directory(input_dir, pattern, output, output_pattern, max, shuff
     else:
         output = Path(output)
 
+    output.mkdir(parents=True, exist_ok=True)
     json_files = list(input_dir.glob(pattern))
 
     # Check for minified files
     search_pattern = output_pattern.format(key="*")
-    minified_files = set(output.rglob(search_pattern))
+    # check if there are already minified files (if the parent path is different)
+    # if the parent path is the same, we will check if the file has been minified, in the main loop
+    # to be more accurate, we actually have to check the patterns as well, but for simplicity, we will just check the parent path, otherwise, let it handle in the main loop
+    if input_dir.resolve().samefile(output.resolve()):
+        minified_files = set()
+    else:
+      minified_files = set(output.rglob(search_pattern))
+    
     minified_files = {f.stem for f in minified_files}
     json_files = [f for f in json_files if f.stem not in minified_files]
 
@@ -51,18 +59,31 @@ def minify_json_directory(input_dir, pattern, output, output_pattern, max, shuff
     total_saved_space = 0
     with tqdm(json_files, desc='📦') as progress:
       for file_path in progress:
+
+          already_minified = False
           file_key = file_path.stem
           output_file_name = Path(output_pattern.format(key=file_key))
           output_file_path = output / output_file_name
 
-          output_file_path.parent.mkdir(parents=True, exist_ok=True)
+          # check if input and output are same (overwrite)
+          if file_path.resolve().samefile(output_file_path.resolve()):
+              # check if the output file has been minified, by checking if it has only one line
+              with output_file_path.open('r') as f:
+                  if len(f.readlines()) == 1:
+                      # skip this file
+                      already_minified = True
 
-          minify_json_file(file_path, output_file_path)
-          saved_space = file_path.stat().st_size - output_file_path.stat().st_size
-          saved_space_mb = saved_space / (1024 * 1024)
-          total_saved_space += saved_space_mb
-          tqdm.write(f"📦 Saved {saved_space_mb:.2f} MB for {output_file_name}")
-          progress.desc = f"📦 Saved {(total_saved_space / 1024):.3f} GB"
+          if already_minified:
+              tqdm.write(f"📦 Skipping {output_file_name} (already minified)")
+          else:
+              output_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+              minify_json_file(file_path, output_file_path)
+              saved_space = file_path.stat().st_size - output_file_path.stat().st_size
+              saved_space_mb = saved_space / (1024 * 1024)
+              total_saved_space += saved_space_mb
+              tqdm.write(f"📦 Saved {saved_space_mb:.2f} MB for {output_file_name}")
+              progress.desc = f"📦 Saved {(total_saved_space / 1024):.3f} GB"
 
 if __name__ == '__main__':
     minify_json_directory()
